8644913a88e731bc4a8e7a05b15b9e3dc62b5752
Shubham Tidke <shubham.tidke.165@gmail.com>
2021-06-19 13:37:26 +0530

    DP codes update!

---
+++ Dynamic Programming/maxProductofThreeNum.java
@@ -0,0 +1,44 @@
+/*
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+Example 1:
+Input: nums = [1,2,3]
+Output: 6
+Example 2:
+Input: nums = [1,2,3,4]
+Output: 24
+Example 3:
+Input: nums = [-1,-2,-3]
+Output: -6
+ */
+import java.util.Collections;
+import java.util.PriorityQueue;
+public class maxProductofThreeNum {
+	public static void main(String[] args) {
+		int num [] = {-1,-2,-3,-4}; 
+		System.out.println(getMaxProduct(num));
+	}
+	private static int getMaxProduct(int[] nums) {
+		PriorityQueue<Integer> poheap = new PriorityQueue<>();
+        PriorityQueue<Integer> neheap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : nums) {
+            poheap.offer(num);
+            neheap.offer(num);
+            if (poheap.size() > 3) {
+                poheap.poll();
+            }
+            if (neheap.size() > 2) {
+                neheap.poll();
+            }
+        }
+        int c1 = 1;
+        int max = 0;
+        while (!poheap.isEmpty()) {
+            max = poheap.poll();
+            c1 *= max;
+        }
+        while (!neheap.isEmpty()) {
+            max *= neheap.poll();
+        }
+        return Math.max(c1, max);
+    }
+}
